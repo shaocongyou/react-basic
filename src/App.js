7e3ed0ff63c4d6295120866ec7edd09e5eba136f
Shaocong You <shaocongyou464@gmail.com>
2025-09-19 23:58:39 +0800

    父传子 通信

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+function Son({ treasure }) {
+    // 接收更语义化的 prop
+    console.log({ treasure });
+    return <div>这是先皇留下的：{treasure}</div>;
+}
 
 function App() {
-  return (
-    <div className="App">
-      this is App
-    </div>
-  );
+    const emperor = '传国玉玺'; // 更语义化的变量名
+    return (
+        <div>
+            <Son treasure={emperor} />
+        </div>
+    );
 }
 
 export default App;
