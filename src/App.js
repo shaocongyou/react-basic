0d27a9a97ca8aaaeef46a4f7e598b30799af67f6
Shaocong You <shaocongyou464@gmail.com>
2025-09-26 02:27:52 +0800

    兄弟组件通信 子传父 父再传给子

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
 import {useState} from "react";
 
-function Son({GetSonMsg}) {
-    const sonMsg = "this is a sonMsg";
+function Son1({sentToFather}) {
+    const [code, setCode] = useState("4399");
     return (
-      <div>
-          <button onClick={() => GetSonMsg(sonMsg)}>send sonMsg to App</button>
-      </div>
+        <div>
+            这是 Son1
+            <button onClick={() => sentToFather(code)}>发送验证码</button>
+        </div>
     );
+}
+
+function Son2({son1Code}) {
+    return <div>这是 Son2，收到的验证码是：{son1Code}</div>;
 }
 
 function App() {
 
-    const [msgFromSon, setMsgFromSon] = useState("");
-    const getSonMsg = (sonMsg) => {setMsgFromSon(sonMsg)}
+    const [code, setCode] = useState("");
+    const GetCode = (son1Code) => {setCode(son1Code)}
 
     return (
         <div>
-            <Son GetSonMsg={getSonMsg} />
-            <span>{msgFromSon}</span>
+            这里是APP的区域
+            <span>
+                <span>Son1 子传父 传递的信息是 验证码</span>
+                <Son1 sentToFather={GetCode} />
+            </span>
+            <span>
+                <Son2 son1Code={code} />
+            </span>
         </div>
     );
 }
