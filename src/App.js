1e0ad5f543b9fa2659c75667257fa7352e0de8d7
Shaocong You <shaocongyou464@gmail.com>
2025-10-01 02:28:00 +0800

    useEffect 概念理解和基础使用

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
+import { useEffect, useState } from "react";
+
+const geekURL = "http://geek.itheima.net/v1_0/channels"
 
 function App() {
-  return (
-    <div className="App">
-      this is App
-    </div>
-  );
+
+    const [res, setRes] = useState({});
+    const [jsonRes, setJsonRes] = useState({});
+
+    useEffect(() => {
+        async function getList(){
+            const response = await fetch(geekURL)
+            const jsonResponse = await response.json()
+            console.log(response)
+            setRes(response)
+            console.log("--------------")
+            console.log(jsonResponse)
+            setJsonRes(jsonResponse)
+        }
+        getList()
+    },[])
+
+    return (
+        <div className="App">
+            <span>Json化 获取到的数据</span>
+            <div>
+                <pre>{JSON.stringify(jsonRes, null, 2)}</pre>
+            </div>
+        </div>
+    );
 }
 
 export default App;
