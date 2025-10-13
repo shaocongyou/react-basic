db13bb7f7eacb43f2bd143ee55157a13bab1dd44
糙米薏仁汤 <shaocongyou464@gamil.com>
2025-10-13 09:06:47 +0000

    useEffect 清除副作用 知识基础

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import { useEffect, useState } from "react";
 
-function App() {
-    
-    const [count, setCount] = useState(0);
-    const [count2, setCount2] = useState(0);
-
-    // 添加特定依赖项
+function Son() {
     useEffect(() => {
-        console.log("🔵 useEffect 执行了！");
-    }, [count]);
-
+      console.log('Son 挂载，设置定时器');
+      
+      const timer = setInterval(() => {
+        console.log('tick - 我还在运行！DOM 在吗？', 
+          document.querySelector('.son-div'));
+      }, 1000);
+      
+      // 注释掉清理函数
+      // return () => clearInterval(timer);
+    }, []);
+    
+    return <div className="son-div">this is son</div>
+  }
+  
+  function App() {
+    const [show, setShow] = useState(true);
+    
     return (
-        <div>
-            <h1>count: {count}</h1>
-            <h1>count2: {count2}</h1>
-            <button onClick={() => setCount(count + 1)}>点击 count 数值 加一</button>
-            <button onClick={() => setCount2(count2 + 1)}>点击 count2 数值加一</button>
-            <h1>打开浏览器控制台（F12），观察useEffect执行次数</h1>
-        </div>
+      <div>
+        {show && <Son />}
+        <button onClick={() => setShow(false)}>卸载Son</button>
+      </div>
     );
-}
+  }
 
 export default App;
